2347d5d46bd63a3fd29d8d0306d8da8146e5e80d
d2gher <d2gher@gmail.com>
2023-02-17 13:06:00 +0200

    A

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: 'Print alphabet except q and e'
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int i;
+	char ch;
+
+	for (i = 0; i < 10; i++)
+		putchar(i + '0');
+
+	for (ch = 'a'; ch <= 'f'; ch++)
+		putchar(ch);
+
+	putchar('\n');
+
+	return (0);
+}
