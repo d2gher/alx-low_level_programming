c58ac5f90badcf2091aa94a57ba3cd9394ec776d
d2gher <d2gher@gmail.com>
2023-02-17 12:32:00 +0200

    A

---
+++ 0x01-variables_if_else_while/4-puts.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <string.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: 'the program's description'
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
