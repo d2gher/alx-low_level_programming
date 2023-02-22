efe596cf4d1abdd8f5668a044d85946ac53fd011
d2gher <d2gher@gmail.com>
2023-02-22 16:33:53 +0200

    A

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -10,15 +10,6 @@
 
 int main(void)
 {
-_putchar('_');
-_putchar('p');
-_putchar('u');
-_putchar('t');
-_putchar('c');
-_putchar('h');
-_putchar('a');
-_putchar('r');
-_putchar('\n');
-
-return (0);
+ print_alphabet();
+ return (0);
 }
