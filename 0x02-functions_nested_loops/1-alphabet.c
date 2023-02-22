801a6add210fcf5787216ecf3e0bc2f16e8709e0
d2gher <d2gher@gmail.com>
2023-02-22 16:41:56 +0200

    A

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,15 +1,12 @@
 #include "main.h"
 
 /**
- * main - Entry point
+ * main - check the code
  *
- * Description: 'Print alphabet'
- *
- * Return: Always 0 (Success)
+ * Return: Always 0.
  */
-
 int main(void)
 {
-print_alphabet();
-return (0);
+ print_alphabet();
+ return (0);
 }
