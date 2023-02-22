270a813149c6ade97f8677802298d8f201545797
d2gher <d2gher@gmail.com>
2023-02-22 18:23:51 +0200

    A

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * _abs - print absolute
+ **@i: number being tested
+ * Return: Always 1 or 0.
+ */
+
+int _abs(int i)
+{
+ if (i < 0)
+		return (i - i - i);
+ return (i);
+}
