7ca666a5705063aceaf920db254cc712fc98744f
d2gher <d2gher@gmail.com>
2023-02-22 17:40:07 +0200

    A

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,12 +1,12 @@
 #include "main.h"
 
 /**
- * print_alphabet - check the code
+ * print_alphabet_x10 - check the code
  *
  * Return: Always 0.
  */
 
-void print_alphabet(void)
+void print_alphabet_x10(void)
 {
  int times = 0;
  while (times > 10)
