2adf921eb4a58c400d7048c89ae8286267b1795f
d2gher <d2gher@gmail.com>
2023-02-22 17:42:17 +0200

    A

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -8,14 +8,14 @@
 
 void print_alphabet_x10(void)
 {
- int times = 0;
- while (times < 10)
- {
-		char i;
+int times = 0;
+while (times < 10)
+{
+	char i;
 
-		for (i = 'a'; i <= 'z'; i++)
-			_putchar(i);
-		_putchar('\n');
-		times++;
- }
+	for (i = 'a'; i <= 'z'; i++)
+		_putchar(i);
+	_putchar('\n');
+	times++;
+}
 }
