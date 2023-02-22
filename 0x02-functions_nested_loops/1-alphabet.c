fddb70a0d5864af45245c774d074164fcce6fd02
d2gher <d2gher@gmail.com>
2023-02-22 17:29:32 +0200

    A

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -5,14 +5,12 @@
  *
  * Return: Always 0.
  */
-int main(void)
-{
- void print_alphabet(void)
- {
-		char i;
 
-		for (i = 'a'; i <= 'z'; i++)
-			_putchar(i);
-		_putchar('\n');
- }
+void print_alphabet(void)
+{
+ char i;
+
+ for (i = 'a'; i <= 'z'; i++)
+		_putchar(i);
+ _putchar('\n');
 }
