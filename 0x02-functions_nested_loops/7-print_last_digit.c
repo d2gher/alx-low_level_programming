c7eb40d3bb953aa27b23435c4e4a841a5886f135
d2gher <d2gher@gmail.com>
2023-02-22 18:40:06 +0200

    A

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * print_last_digit - print last digit of a number
+ **@i: number being tested
+ * Return: a number.
+ */
+
+int print_last_digit(int i)
+{
+ return (i % 10);
+}
