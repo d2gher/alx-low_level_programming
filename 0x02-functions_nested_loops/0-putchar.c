1d8acc1f933fca2461faf9832e9a9efaefcd178d
d2gher <d2gher@gmail.com>
2023-02-22 16:37:51 +0200

    A

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -3,7 +3,7 @@
 /**
  * main - Entry point
  *
- * Description: 'Print alphabet'
+ * Description: 'Print _putchar'
  *
  * Return: Always 0 (Success)
  */
--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -10,6 +10,6 @@
 
 int main(void)
 {
- print_alphabet();
- return (0);
+print_alphabet();
+return (0);
 }
