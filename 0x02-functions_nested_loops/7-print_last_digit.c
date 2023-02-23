c0667c74cabb043be6a67fdc3b58a94c33d2f292
d2gher <d2gher@gmail.com>
2023-02-23 10:40:00 +0200

    A

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -8,14 +8,15 @@
 
 int print_last_digit(int i)
 {
- int a;
- if (i < 0)
-		i = -i;
+int a;
+if (i < 0)
+	i = -i;
 
- a = i % 10;
+a = i % 10;
 
- if (a < 0)
-		a = -a;
- _putchar(a + '0');
- return (a);
+if (a < 0)
+	a = -a;
+
+_putchar(a + '0');
+return (a);
 }
