c747727f686ccf63908eb098c0141c1c270e16b6
d2gher <d2gher@gmail.com>
2023-02-23 17:11:28 +0200

    A

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -10,18 +10,18 @@ void main(void)
  int i;
  for (i = 1; i <= 100; i++)
  {
-		if (1)
+		if ((i % 3 != 0) || (i % 3 != 0))
 		{
 			printf("%d", i);
 		}
 		else
 		{
 			if (i % 3 == 0)
-				printf("Fizz");
-			if (i % 5 == 0)
-				printf("Buzz");
+				// printf("Fizz");
+				if (i % 5 == 0)
+			// printf("Buzz");
 		}
-		printf(" ");
+		// printf(" ");
  }
  printf("\n");
 }
