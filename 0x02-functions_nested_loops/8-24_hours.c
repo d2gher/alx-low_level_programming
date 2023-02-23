80c7010765c3d5cfbbb6226fe0a2018ffae7f571
d2gher <d2gher@gmail.com>
2023-02-23 10:46:29 +0200

    A

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * print_last_digit - print last digit of a number
+ **@i: number being tested
+ * Return: a number.
+ */
+
+void jack_bauer(void);
+{
+ for (int h = 0; h < 25; h++)
+ {
+		for (int m = 0; m < 61; m++)
+		{
+			if (h < 10)
+				_putchar('0');
+			_putchar(h + '0');
+			_putchar(':');
+			if (m < 10)
+				_putchar('0');
+			_putchar(m + '0');
+		}
+ }
+}
