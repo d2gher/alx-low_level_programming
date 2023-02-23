411a94758f4e08eb8a74acb1cebc705f379b8e61
d2gher <d2gher@gmail.com>
2023-02-23 11:16:30 +0200

    A

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -7,27 +7,27 @@
 
 void times_table(void)
 {
- int f, l, n;
- for (f = 0; f < 10; f++)
- {
-		for (l = 0; l < 10; l++)
+int f, l, n;
+for (f = 0; f < 10; f++)
+{
+	for (l = 0; l < 10; l++)
+	{
+		n = f * l;
+		if (l > 0)
 		{
-			n = f * l;
-			if (l > 0)
-			{
-				if ((n / 10) < 1)
-					_putchar(' ');
-				else
-					_putchar((n / 10) + '0');
-			}
-
-			_putchar((n % 10) + '0');
-			if (l < 9)
-			{
-				_putchar(',');
+			if ((n / 10) < 1)
 				_putchar(' ');
-			}
+			else
+				_putchar((n / 10) + '0');
 		}
-		_putchar('\n');
- }
+
+		_putchar((n % 10) + '0');
+		if (l < 9)
+		{
+			_putchar(',');
+			_putchar(' ');
+		}
+	}
+	_putchar('\n');
+}
 }
