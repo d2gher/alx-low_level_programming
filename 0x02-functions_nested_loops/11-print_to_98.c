2cca172505a5fabb29241c840e1a3a90cc629d6d
d2gher <d2gher@gmail.com>
2023-02-23 11:26:26 +0200

    A

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+ * print_to_98 - print from n to 98
+ **@n: number to print from
+ * Return: a number.
+ */
+
+void print_to_98(int n)
+{
+ int i;
+ if (n <= 98)
+ {
+		for (i = n; i <= 98; i++)
+		{
+			_putchar(i);
+			_putchar(',');
+			_putchar(' ');
+		}
+ }
+ else
+ {
+		for (i = n; i >= 98; i--)
+		{
+			_putchar(i);
+			_putchar(',');
+			_putchar(' ');
+		}
+ }
+ _putchar('\n')
+}
