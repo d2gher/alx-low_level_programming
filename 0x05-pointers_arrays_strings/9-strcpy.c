f6d54b8824321957aa3e1371fb97ae54cb3fb38c
d2gher <d2gher@gmail.com>
2023-02-27 18:04:03 +0200

    A

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -9,12 +9,12 @@
  */
 char *_strcpy(char *dest, char *src)
 {
+ int i;
  int l = 0;
  char *p = src;
  while (*(p + l) != '\0')
 		l++;
 
- int i;
  for (i = 0; i < l; i++)
 		*(dest + i) = *(src + i);
 }
