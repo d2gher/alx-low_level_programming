6795f169387a602851ab5102600f225a357e0fad
d2gher <d2gher@gmail.com>
2023-02-27 18:08:02 +0200

    A

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -1,21 +1,18 @@
-#include "main.h"
-#include <stdio.h>
-
 /**
- * print_array - Prints an n number of elements of arrays
- * @a: The array
- * @n: Number of elemets to print
- * Return: nothing
+ * _strcpy - Copy string
+ * @dest: The array
+ * @src: Number of elemets to print
+ * Return: Address for new copy
  */
 char *_strcpy(char *dest, char *src)
 {
- int i;
- int l = 0;
- char *p = src;
- while (*(p + l) != '\0')
-		l++;
+int i;
+int l = 0;
+char *p = src;
+while (*(p + l) != '\0')
+	l++;
 
- for (i = 0; i < l; i++)
-		*(dest + i) = *(src + i);
- return (dest);
+for (i = 0; i <= l; i++)
+	*(dest + i) = *(src + i);
+return (dest);
 }
