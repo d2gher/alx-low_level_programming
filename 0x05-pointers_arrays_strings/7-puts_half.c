ec98e95c30d24f60d05560707dafe9d80f918ae9
d2gher <d2gher@gmail.com>
2023-02-27 17:36:12 +0200

    A

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -13,7 +13,15 @@ void puts_half(char *str)
 		l++;
 
  if (l == 0)
+ {
+		_putchar('\n');
 		return;
+ }
+
+ if (l % 2 == 0)
+		i = l / 2;
+ else
+		i = (l - 1) / 2;
 
  for (i = l / 2; i < l; i++)
 		_putchar(str[i]);
