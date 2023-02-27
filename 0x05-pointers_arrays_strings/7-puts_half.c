d97df2faa48136fda4cd84b417333777a151def0
d2gher <d2gher@gmail.com>
2023-02-27 17:39:14 +0200

    A

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * puts2 - Prints every other character of a string
+ * puts_half - Prints second half of a string
  * @str: A string
  * Return: nothing
  */
@@ -18,12 +18,8 @@ void puts_half(char *str)
 		return;
  }
 
- if (l % 2 == 0)
-		i = l / 2;
- else
-		i = (l - 1) / 2;
-
- for (i = l / 2; i < l; i++)
+ i = (l - 1) / 2;
+ for (i = i + 1; i < l; i++)
 		_putchar(str[i]);
  _putchar('\n');
 }
