0429f05cb8a2d9eaab1c9a64c805df7b53e335e9
d2gher <d2gher@gmail.com>
2023-03-01 21:37:30 +0200

    l

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,21 @@
+/**
+ * _strncpy - add n chars of a string to string
+ * @dest: First string
+ * @src: Last string
+ * @n: Length of chars to add
+ * Return: returns full string.
+ */
+char *_strncpy(char *dest, char *src, int n)
+{
+int i;
+i = 0;
+
+while (src[j] != '\0')
+{
+dest[i] = src[j];
+i++;
+}
+dest[i] = '\0';
+
+return (dest);
+}
