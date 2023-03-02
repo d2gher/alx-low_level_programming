4995302198812d5ccd0c6303f2a167c1410ecb42
d2gher <d2gher@gmail.com>
2023-03-02 09:51:42 +0200

    l

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -7,19 +7,20 @@ char *cap_string(char *s)
 {
 int i, j, to_up;
 to_up = 'a' - 'A';
-char Separators[13] = {' ', '\t', '\n', ',', ';', '.', '!', '?', '"', '(', ')', '{', '}'};
+char Separators[13] = {' ', '\t', '\n', ',', ';', '.',
+'!','?', '"', '(', ')', '{', '}'};
 
 for (i = 0; s[i] != '\0' ; i++)
 {
 	if (i == 0 && s[i] >= 'a' && s[i] <= 'z')
-		s[i] = s[i] - to_up;
+		s[i] -= to_up;
 
 	for (j = 0; j < 13; j++)
 	{
 		if (Separators[j] == s[i])
 		{
 			if (s[i + 1] >= 'a' && s[i + 1] <= 'z')
-				s[i + 1] = s[i + 1] - to_up;
+				s[i + 1] -= to_up;
 		}
 	}
 }
