18d66a73c7d6817b5e7cc2d28bda2453c9189811
d2gher <d2gher@gmail.com>
2023-03-02 08:31:33 +0200

    l

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -1,18 +1,21 @@
 /**
- * _strcmp - Compare two stings
- * @s1: First string
- * @s2: last string
- * Return: Returns 0 if the strings are equal, or subtracts them from each other
+ * _strcmp - compares two strings
+ * @s1: first string to compare
+ * @s2: second string to compare
+ *
+ * Return: less than 0 if s1 is less than s2, 0 if they're equal,
+ * more than 0 if s1 is greater than s2
  */
 int _strcmp(char *s1, char *s2)
 {
-while (true)
-{
-if (*s1 == '\0')
-	return (0);
-if (*s1 != *s2)
+	while (*s1 == *s2)
+	{
+		if (*s1 == '\0')
+		{
+			return (0);
+		}
+		s1++;
+		s2++;
+	}
 	return (*s1 - *s2);
-s1++;
-s2++;
-}
 }
