f2604c7c9f0a365a9f56b0e18e6dd49782f9d2fa
d2gher <d2gher@gmail.com>
2023-03-02 10:00:57 +0200

    l

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,22 @@
+/**
+ * leet - Encode string with leet
+ * @s: The string
+ * Return: The encoded string
+ */
+char *leet(char *s)
+{
+int i, j;
+char *a = "aAeEoOtTlL";
+char *b = "4433007711";
+
+for (i = 0; s[i] != '\0'; i++)
+{
+	for (j = 0; j < 10; j++)
+	{
+		if (s[i] == a[j])
+			s[i] = b[j];
+	}
+}
+
+return (s);
+}
