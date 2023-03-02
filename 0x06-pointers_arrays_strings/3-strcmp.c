20558b547e14a6784ca8a8bf998e617e43e568ce
d2gher <d2gher@gmail.com>
2023-03-02 08:28:03 +0200

    l

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -8,10 +8,10 @@ int _strcmp(char *s1, char *s2)
 {
 while (true)
 {
-if (s1 == '\0')
+if (*s1 == '\0')
 	return (0);
-if (s1 != s2)
-	return (s1 - s2);
+if (*s1 != s2)
+	return (*s1 - *s2);
 s1++;
 s2++;
 }
