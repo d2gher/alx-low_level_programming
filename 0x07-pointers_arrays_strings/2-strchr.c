eca5403002d46235c7248ab5bf5012531602e587
d2gher <d2gher@gmail.com>
2023-03-06 21:21:35 +0200

    A

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -7,12 +7,18 @@
  */
 char *_strchr(char *s, char c)
 {
+int a;
+
 while (1)
 {
-if (*s == c)
-	return (s);
-if (*s == 0)
+a = *s++;
+if (a == c)
+{
+	return (s - 1);
+}
+if (a == 0)
+{
 	return (NULL);
-s++;	
+}
 }
 }
