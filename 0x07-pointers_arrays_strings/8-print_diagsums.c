a36a70ae5ae294cd31e5e190b4d83b416053e9b0
d2gher <d2gher@gmail.com>
2023-03-06 22:37:59 +0200

    A

--- 0x07-pointers_arrays_strings/8-print_diagsums.c
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -1,4 +1,3 @@
-#include "main.h"
 #include <stdio.h>
 
 /**
@@ -14,7 +13,7 @@ sum2 = 0;
 
 for (i = 0; i < size; i++) {
 	sum1 += a[(size * i) + i];
-	sum1 += a[(size * (i + 1)) - (i + 1)];
+	sum2 += a[(size * (i + 1)) - (i + 1)];
 }
-printf("%d, %d\n", sum, sum1);
+printf("%d, %d\n", sum1, sum2);
 }
