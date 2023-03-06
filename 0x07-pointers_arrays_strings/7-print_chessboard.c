41e6507c3e5425db40511ed077185ce2ef4f1d68
d2gher <d2gher@gmail.com>
2023-03-06 22:19:05 +0200

    A

--- 0x07-pointers_arrays_strings/7-print_chessboard.c
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -2,10 +2,9 @@
 
 /**
  * *print_chessboard - print 2 dimensional array
- * @haystack: string to search in
+ * @a: array to print
  *
- * Return: pointer to the beginning of the located substring
- * or NULL if the substring is not found
+ * Return: Nothing
  */
 void print_chessboard(char (*a)[8])
 {
@@ -14,9 +13,7 @@ int i, j;
 for (i = 0; i < 8; i++)
 {
 	for (j = 0; j < 8; j++)
-	{
 		_putchar(a[i][j]);
-	}
 	_putchar('\n');
 }
 }
