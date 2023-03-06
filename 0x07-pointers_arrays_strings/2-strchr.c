98ad6c8a9d04ad84ade53c9665a335f75ec17f71
d2gher <d2gher@gmail.com>
2023-03-06 21:02:52 +0200

    A

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -9,8 +9,12 @@ char *_strchr(char *s, char c)
 unsigned int i;
 
 i = 0;
-while(s[i] != "\0")
-	if (s[i] == c)
-		return (&s[i]);
+while(s[i] != "\0") 
+{
+if (s[i] == c)
+	return (&s[i]);
+i++;	
+}
+	
 return (NULL);
 }
