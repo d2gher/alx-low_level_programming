6945bc4b9322ef4370310c4066963168f1c31ca5
d2gher <d2gher@gmail.com>
2023-03-06 20:59:41 +0200

    A

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,16 @@
+/**
+ * _strchr - Find char in a string
+ * @s: String
+ * @c: Char to find
+ * Return: Pointer to char if found, or null
+ */
+char *_strchr(char *s, char c)
+{
+unsigned int i;
+
+i = 0;
+while(s[i] != "\0")
+	if (s[i] == c)
+		return (&s[i]);
+return (NULL)
+}
