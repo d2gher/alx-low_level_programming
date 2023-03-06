cb7bbcd97376816d1cfdde1e626e0ff04baf109e
d2gher <d2gher@gmail.com>
2023-03-06 21:37:29 +0200

    A

--- 0x07-pointers_arrays_strings/4-strpbrk.c
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -9,19 +9,17 @@
  */
 char *_strpbrk(char *s, char *accept)
 {
-	int i, j;
+int i, j;
 
-	for (i = 0; *s != '\0'; i++)
+for (i = 0; *s != '\0'; i++)
+{
+	for (j = 0; accept[j] != '\0'; j++)
 	{
-		for (j = 0; accept[j] != '\0'; j++)
-		{
-			if (*s == accept[j])
-			{
-				return (s);
-			}
-		}
-		s++;
+		if (*s == accept[j])
+			return (s);
 	}
+	s++;
+}
 
-	return (NULL);
+return (NULL);
 }
