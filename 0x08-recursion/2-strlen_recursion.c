b543e8295f3d7439b4aae752e58729920553cb04
d2gher <d2gher@gmail.com>
2023-03-08 19:32:46 +0200

    Recursion

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,12 @@
+/**
+ * _strlen_recursion - gets string length, recursively
+ * @s: string
+ *
+ * Return: length of the string
+ */
+int _strlen_recursion(char *s)
+{
+if (*s != '\0')
+	return 0;
+return 1 + _strlen_recursion(s + 1);
+}
