eb0a0c9efc8537e485d24d645ee67030246c782e
d2gher <d2gher@gmail.com>
2023-03-10 21:12:46 +0200

    Arguments

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -55,18 +55,19 @@ int _atoi(char *s)
  */
 int main(int argc, char *argv[])
 {
-	if (argc == 3)
+	int result, num1, num2;
+
+	if (argc < 3 || argc > 3)
 	{
-		int result, num1, num2;
-		num1 = _atoi(argv[1]);
-		num2 = _atoi(argv[2]);
-		result = num1 * num2;
-
-		printf("%d\n", result);
-
-		return (0);
+		printf("Error\n");
+		return (1);
 	}
 
-	printf("Error\n");
-	return (1);
+	num1 = _atoi(argv[1]);
+	num2 = _atoi(argv[2]);
+	result = num1 * num2;
+
+	printf("%d\n", result);
+
+	return (0);
 }
