102cc51e9951f2b7dd1265db0517a76c4cf073c2
d2gher <d2gher@gmail.com>
2023-03-13 15:54:21 +0200

    Malloc

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -2,7 +2,8 @@
 #include <stdlib.h>
 /**
  * str_concat - concat two strings
- * @s: strings to concat
+ * @s1: first string
+ * @s2: second string
  *
  * Return: pointer to new string
  */
@@ -24,7 +25,7 @@ while (s1[len1])
 while (s2[len2])
 	len2++;
 
-size = len1 + len2; 
+size = len1 + len2;
 
 newstr = malloc(sizeof(char) * (size + 1));
 
@@ -35,12 +36,12 @@ m = 0;
 for (i = 0; s1[i] != '\0'; i++)
 {
 	newstr[m] = s1[i];
-	m++; 
+	m++;
 }
 for (i = 0; s2[i] != '\0'; i++)
 {
 	newstr[m] = s2[i];
-	m++; 
+	m++;
 }
 
 newstr[m + 1] = '\0';
