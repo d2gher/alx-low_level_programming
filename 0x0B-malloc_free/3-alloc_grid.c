3cc71e9d0e4be60f17e3e4918883a2333f5bb2cc
d2gher <d2gher@gmail.com>
2023-03-13 16:09:02 +0200

    Malloc

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -24,7 +24,12 @@ for (i = 0; i < height; i++)
 {
 	arr[i] = (int *) malloc(sizeof(int) * width);
 	if (arr[i] == NULL)
+	{
+		free(arr);
+		for (j = 0; j <= i; j++)
+			free(arr[j]);
 		return (NULL);
+	}
 }
 
 for (i = 0; i < height; i++)
