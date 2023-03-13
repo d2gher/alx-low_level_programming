b72e969b6070ccb79cd849b10a0cab4d6346c31c
d2gher <d2gher@gmail.com>
2023-03-13 15:18:50 +0200

    Malloc

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -13,12 +13,14 @@ if (size == 0)
 	return (NULL);
 
 char *arr;
+unsigned int i;
+
 arr = (char *) malloc(sizeof(char) * size);
 
 if (arr == NULL)
 	return (0);
 
-int i;
+
 for (i = 0; i <= size; i++)
 	arr[i] = c;
 
