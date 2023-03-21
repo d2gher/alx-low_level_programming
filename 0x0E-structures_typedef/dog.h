492c51a33554f956fe96444a53ec425e6ed669b3
d2gher <d2gher@gmail.com>
2023-03-21 16:49:25 +0200

    0x0D

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,14 @@
+/**
+ * struct dog - Hold a dog's information
+ * @name: dog's name
+ * @age: dog's age
+ * @owner: dog's owner
+ * 
+ * Description: Longer description
+ */
+struct dog 
+{
+char *name;
+float age;
+char *owner;
+};
