ac1816a8262a99734c6b074a635436078605e60a
d2gher <d2gher@gmail.com>
2023-03-21 17:41:23 +0200

    0x0D

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -32,12 +32,19 @@ int _strlen(char *s)
  */
 char *_strcpy(char *dest, char *src)
 {
-	int i;
+	int len, i;
 
-	i = 0;
-	while (src[++i] != '\0')
+	len = 0;
+
+	while (src[len] != '\0')
+	{
+		len++;
+	}
+
+	for (i = 0; i < len; i++)
+	{
 		dest[i] = src[i];
-
+	}
 	dest[i] = '\0';
 
 	return (dest);
