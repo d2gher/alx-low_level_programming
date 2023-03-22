aa1711fe25fb038369cec08bca9d8838f5bb0df5
d2gher <d2gher@gmail.com>
2023-03-22 19:48:24 +0200

    pointers to functions

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -10,7 +10,7 @@
 void array_iterator(int *array, size_t size, void (*action)(int))
 {
 	unsigned int i = 0;
-	if (!array || !size || !action)
+	if (!array || !action)
 		return;
 	for (i = 0; i < size; i++)
 		actions(array[i]);
