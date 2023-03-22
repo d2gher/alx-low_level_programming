620053680ced9700a25d571ac97d3fadd42a1a10
d2gher <d2gher@gmail.com>
2023-03-22 19:57:25 +0200

    pointers to functions

---
+++ 0x0F-function_pointers/2-int_index.c
@@ -0,0 +1,24 @@
+#include <stdlib.h>
+#include "function_pointers.h"
+
+/**
+ * int_index - find a value in array
+ *
+ * @array: array to iterate over
+ * @size: size of the array
+ * @cmp: function to compare with
+ */
+int int_index(int *array, int size, int (*cmp)(int))
+{
+	size_t i;
+
+	if (!array || !size || !cmp)
+		return (-1);
+
+	for (i = 0; i < size; i++)
+	{
+		if (cop(array[i]))
+			return i;
+	}
+	return (-1);
+}
