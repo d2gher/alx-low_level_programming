3e9ee5c312102fca643de1f6938191fb023c2854
d2gher <d2gher@gmail.com>
2023-04-03 15:58:06 +0200

    Lists

---
+++ 0x13-more_singly_linked_lists/lists.h
@@ -0,0 +1,21 @@
+#ifndef VAR_LISTS
+#define VAR_LISTS
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * struct listint_s - singly linked list
+ * @n: integer
+ * @next: points to the next node in the list
+ *
+ * Description: singly linked list node structure
+ */
+typedef struct listint_s
+{
+	int n;
+	struct listint_s *next;
+} listint_t;
+
+size_t print_listint(const listint_t *h);
+
+#endif
